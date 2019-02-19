ce2766ebd4086fe8bc3c8b0d11f894a95b2629b0
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-02-19 18:03:54 -0500

    Created get user by id route. Setup error handler for user errors.

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -4,14 +4,33 @@ const Users = require('./userDb');
 
 const usersRouter = express.Router();
 
-usersRouter.get('/', async (req, res) => {
+usersRouter.get('/', async (req, res, next) => {
     try {
         const users = await Users.get();
-        res.status(200).json(users)
+        res.status(200).json(users);
     } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: 'Error retrieving the users.'})
+        next({code: 500, action: 'getting', subject: 'users'});
     }
 });
+
+usersRouter.get('/:id', async (req, res, next) => {
+    const userID = req.params.id;
+    try {
+        const user = await Users.getById(userID);
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            next({code: 404, action: 'getting', subject: 'user by specified ID.'});
+        }
+    } catch (err) {
+        next({code: 500, action: 'getting', subject: 'user'});
+    }
+});
+
+usersRouter.use(errorHandlerUser);
+
+function errorHandlerUser(err, req, res, next) {
+    res.status(err.code).json({ errorMessage: `Error ${err.action} the ${err.subject}` });
+}
 
 module.exports = usersRouter;
