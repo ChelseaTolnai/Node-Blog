809d1370e40fff3fdd95d6ea593387d503a163bf
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-02-19 18:39:22 -0500

    Created post function for user with toUpperCase middleware for name. Function confirmed user name is given in request and that user name is unique.

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -6,7 +6,7 @@ const usersRouter = express.Router();
 
 usersRouter.get('/', async (req, res, next) => {
     try {
-        const users = await Users.get();
+        const users = await Users.get(req.query);
         res.status(200).json(users);
     } catch (err) {
         next({code: 500, action: 'getting', subject: 'users'});
@@ -27,10 +27,37 @@ usersRouter.get('/:id', async (req, res, next) => {
     }
 });
 
+usersRouter.post('/', nameUpperCase, async (req, res, next) => {
+    try {
+        const users = await Users.get(req.query);
+        const userNames = users.map(user => user.name.toUpperCase());
+        if (userNames.includes(userNameCap)) {
+            next({code: 400, action: 'adding', subject: 'user. User name is already taken'})
+            return;
+        } else {
+            const user = await Users.insert({...req.body, name: userNameCap});
+            res.status(201).json(user);
+        }
+    } catch (err) {
+        next({code: 500, action: 'adding', subject: 'user'});
+    }
+});
+
+function nameUpperCase(req, res, next) {
+    if (!req.body.name) {
+        next({code: 400, action: 'adding', subject: 'user. User name is required'})
+        return;
+    } else {
+        userNameCap = req.body.name.toUpperCase();
+        console.log(userNameCap);
+        next();
+    }
+};
+
 usersRouter.use(errorHandlerUser);
 
 function errorHandlerUser(err, req, res, next) {
-    res.status(err.code).json({ errorMessage: `Error ${err.action} the ${err.subject}` });
+    res.status(err.code).json({ errorMessage: `Error ${err.action} the ${err.subject}.` });
 }
 
 module.exports = usersRouter;
