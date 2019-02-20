1f2ea7f57e17435b42b4677047cf831fce7eead6
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-02-20 01:14:04 -0500

    Added dotenv to project and start node script to deploy site.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // code away!
+require('dotenv').config();
+
 const server = require('./server');
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on port ${port} ***\n`);
